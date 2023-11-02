6f0bc5aff077f0622b57048d1e64c12e69ca1657
skwasjer <11424653+skwasjer@users.noreply.github.com>
2023-11-02 22:08:58 +0100

    feat: initial commit

---
+++ src/GhActions/Greeter.cs
@@ -0,0 +1,17 @@
+﻿namespace GhActions;
+
+/// <summary>
+/// Greeter
+/// </summary>
+public static class Greeter
+{
+    /// <summary>
+    /// Hello
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string Hello(string name)
+    {
+        return $"Hello {name}";
+    }
+}
---
+++ test/GhActions.Tests/GreeterTests.cs
@@ -0,0 +1,10 @@
+﻿namespace GhActions.Tests;
+
+public class GreeterTests
+{
+    [Fact]
+    public void Test()
+    {
+        Assert.Equal("Hello me", Greeter.Hello("me"));
+    }
+}
